cc8cdce93bd39ab59bc01766ecbde08ed1000641
voidFunctionReturn0 <voidFunctionReturn0@gmail.com>
2023-09-23 14:56:14 +0900

    입금, 출금 입력값이 숫자인지 검사하는 기능 추가

--- atm.ex
+++ atm.ex
@@ -30,49 +30,69 @@ defmodule ATM do
 
   def deposit() do
     # 입금 금액 입력
-    amount = IO.gets("입금하실 금액을 입력해주세요. ")
+    input = IO.gets("입금하실 금액을 입력해주세요. ")
     |> delete_newline_from_input()
 
-    # 잔액 조회
-    {:ok, balance} = File.read(@balance_path)
+    # 입력값이 숫자인지 검사
+    parsed_input = Integer.parse(input)
+    if (parsed_input == :error) do
+      IO.puts("[입력 오류]")
+      IO.puts("숫자를 입력해주세요.\n")
+      deposit()
+    else
+      {amount, _} = parsed_input
 
-    # 잔액 + 입금 금액
-    sum = String.to_integer(balance) + String.to_integer(amount)
+      # 잔액 조회
+      {:ok, balance} = File.read(@balance_path)
 
-    # 합한 금액 저장
-    {:ok, file} = File.open(@balance_path, [:write])
-    IO.binwrite(file, "#{sum}")
-    File.close(file)
+      # 잔액 + 입금 금액
+      sum = String.to_integer(balance) + amount
 
-    # 입금 후 잔액 보여주기
-    IO.puts("입금 후 잔액: #{sum}\n")
+      # 합한 금액 저장
+      {:ok, file} = File.open(@balance_path, [:write])
+      IO.binwrite(file, "#{sum}")
+      File.close(file)
+
+      # 입금 후 잔액 보여주기
+      IO.puts("입금 후 잔액: #{sum}\n")
+    end
   end
 
   def withdraw() do
     # 출금 금액 입력
-    amount = IO.gets("출금하실 금액을 입력해주세요. ")
+    input = IO.gets("출금하실 금액을 입력해주세요. ")
     |> delete_newline_from_input()
 
-    # 잔액 조회
-    {:ok, balance} = File.read(@balance_path)
-
-    # 잔액 - 출금 금액
-    subtracted_amount = String.to_integer(balance) - String.to_integer(amount)
-
-    # 잔액이 출금 금액보다 적은지 확인
-    if (subtracted_amount < 0) do
-      # 잔액 부족한 경우
-      IO.puts("[잔액 부족]")
-      IO.puts("잔액이 부족합니다.\n")
+    # 입력값이 숫자인지 검사
+    parsed_input = Integer.parse(input)
+    if (parsed_input == :error) do
+      IO.puts("[입력 오류]")
+      IO.puts("숫자를 입력해주세요.\n")
       withdraw()
     else
-      # 출금 성공
-      {:ok, file} = File.open(@balance_path, [:write])
-      IO.binwrite(file, "#{subtracted_amount}")
-      File.close(file)
+      {amount, _} = parsed_input
 
-      # 출금 후 잔액 보여주기
-      IO.puts("입금 후 잔액: #{subtracted_amount}\n")
+      # 잔액 조회
+      {:ok, balance} = File.read(@balance_path)
+
+      # 잔액 - 출금 금액
+      subtracted_amount = String.to_integer(balance) - amount
+
+      # 잔액이 출금 금액보다 적은지 확인
+      if (subtracted_amount < 0) do
+        # 잔액 부족한 경우
+        IO.puts("[잔액 부족]")
+        IO.puts("잔액이 부족합니다.\n")
+        withdraw()
+      else
+        # 출금 성공
+        {:ok, file} = File.open(@balance_path, [:write])
+        IO.binwrite(file, "#{subtracted_amount}")
+        File.close(file)
+
+        # 출금 후 잔액 보여주기
+        IO.puts("입금 후 잔액: #{subtracted_amount}\n")
+      end
     end
   end
 
